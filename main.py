104e523b78f7ccdf12740bf7de03109f9377bd99
Андрей <poyarkov12390@gmail.com>
2020-11-30 10:45:47 +0300

    Капучино

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QPainter, QColor
 from random import choice, randint
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QTableWidget
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sqlite3
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQueryModel
@@ -14,10 +14,26 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+class AddCoffee(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.setLayout(self.main)
+
+
 class Form(QWidget):
     def __init__(self):
         super().__init__()
+        self.main = QVBoxLayout()
         uic.loadUi("main.ui", self)
+        self.update_table()
+        self.btn = QPushButton("Добавить")
+        self.btn.clicked.connect(self.add_coffee)
+        self.main.addWidget(self.Table)
+        self.main.addWidget(self.btn)
+        self.setLayout(self.main)
+
+    def update_table(self):
         db = QSqlDatabase.addDatabase('QSQLITE')
         db.setDatabaseName('coffee.sqlite')
         db.open()
@@ -25,6 +41,32 @@ class Form(QWidget):
         model.setTable('coffee')
         model.select()
         self.Table.setModel(model)
+
+    def add_coffee(self):
+        self.add = AddCoffee()
+        self.add.show()
+        self.add.pushButton.clicked.connect(self.coffee_to_db)
+
+    def coffee_to_db(self):
+        name_sort = self.add.lineEdit.text()
+        degree = self.add.lineEdit_2.text()
+        ground_grains = self.add.comboBox.currentText()
+        taste = self.add.lineEdit_3.text()
+        cost = self.add.lineEdit_4.text()
+        volume = self.add.lineEdit_5.text()
+        link = sqlite3.connect("coffee.sqlite")
+        cursor = link.cursor()
+        query = cursor.execute("""INSERT INTO `coffee`(`name_sort`, `degree of roasting`, `ground / in grains`,
+         `the description of the taste`, `cost`, `volume`)
+        VALUES (?, ?, ?, ?, ?, ?)""", (str(name_sort), str(degree), str(ground_grains), str(taste), int(cost), int(volume)))
+        link.commit()
+        self.add.lineEdit.clear()
+        self.add.lineEdit_2.clear()
+        self.add.lineEdit_3.clear()
+        self.add.lineEdit_4.clear()
+        self.add.lineEdit_5.clear()
+        self.add.hide()
+        self.update_table()
 
 
 if __name__ == '__main__':
